3b9e505df429dad9d2410259505085ad8145ddcf
Dzhin2001 <Dzhin2001@mail.ru>
2022-02-21 22:31:58 +0300

    две функции

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-let getRandomInt = function(min,max)
+const getRandomInt = function(min,max)
 {
   return Math.round(Math.random()*(max+1-min)+min);
 };
 
-let validStrLen = function(str, maxLen)
+const validStrLen = function(str, maxLen)
 {
-  return str.length <= maxLen
+  return str.length <= maxLen;
 };
 
 getRandomInt(5,10);
