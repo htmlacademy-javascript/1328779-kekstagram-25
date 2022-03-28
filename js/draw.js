3228eadd429c0de4f7e4de06e220436dbdbd662c
Димитар <dzhin2001@mail.ru>
2022-03-28 12:05:55 +0300

    Добавление фото

--- js/draw.js
+++ js/draw.js
@@ -5,13 +5,13 @@ const drawPhotos = (Photos) => {
 
   const fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < Photos.length; i++) {
+  Photos.forEach( (Photo) => {
     const element = Template.cloneNode(true);
-    element.querySelector('.picture__img').src = Photos[i].url;
-    element.querySelector('.picture__likes').textContent = Photos[i].likes;
-    element.querySelector('.picture__comments').textContent = Photos[i].comments.length;
+    element.querySelector('.picture__img').src = Photo.url;
+    element.querySelector('.picture__likes').textContent = Photo.likes;
+    element.querySelector('.picture__comments').textContent = Photo.comments.length;
     fragment.appendChild(element);
-  }
+  });
 
   Section.appendChild(fragment);
   return fragment;
